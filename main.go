ca8bafb3834c35d8f5534b482ab8a4ab3a994fb5
Kazuhisa Togo <kazuhisa@ktogo.net>
2019-06-14 20:55:39 +0900

    Refactor main.go

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/ktogo/terraformer-plan-splitter/cmd"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
-		log.Println(err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 }
