dbc33c5ff6c330ee61cd7c4132daba70cca5390e
Kazuhisa Togo <kazuhisa@ktogo.net>
2019-06-14 21:44:31 +0900

    Let split command parse planfile

--- cmd/split.go
+++ cmd/split.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/ktogo/terraformer-plan-splitter/plan"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +12,12 @@ func newCmdSplit() *cobra.Command {
 		Use:  "split",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return fmt.Errorf("Testing. First arg is %s", args[0])
+			plan, err := plan.LoadPlanfile(args[0])
+			if err != nil {
+				return err
+			}
+			fmt.Println(fmt.Sprintf("%#v\n", plan))
+			return nil
 		},
 		SilenceUsage:  true,
 		SilenceErrors: false,
