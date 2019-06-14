81e7c1e9db7eef44c4e6683be3ffa85dd3d26bda
Kazuhisa Togo <kazuhisa@ktogo.net>
2019-06-15 03:40:00 +0900

    Implement splitting featurue

--- cmd/split.go
+++ cmd/split.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	terraformer_cmd "github.com/GoogleCloudPlatform/terraformer/cmd"
+	"github.com/GoogleCloudPlatform/terraformer/terraform_utils"
+	"github.com/ktogo/terraformer-plan-modifier/utils/resourcemapper"
 	"github.com/spf13/cobra"
 )
 
@@ -12,14 +14,47 @@ func newCmdSplit() *cobra.Command {
 		Use:  "split",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			plan, err := terraformer_cmd.LoadPlanfile(args[0])
-			if err != nil {
-				return err
-			}
-			fmt.Println(fmt.Sprintf("%#v\n", plan))
-			return nil
+			return splitResources(args[0])
 		},
 		SilenceUsage:  true,
 		SilenceErrors: false,
 	}
+}
+
+func splitResources(path string) error {
+	plan, err := terraformer_cmd.LoadPlanfile(path)
+	if err != nil {
+		return err
+	}
+
+	mg := new(resourcemapper.Generator)
+	mg.Add("example", `^(?:.+\.)?example\.com$`, `my-?example`)
+
+	ms, err := mg.Compile()
+	if err != nil {
+		return err
+	}
+	ms.DefaultName = "default"
+
+	resourceMap := map[string][]terraform_utils.Resource{}
+
+	for _, resource := range plan.Resources {
+		name, _ := resource.InstanceState.Attributes["name"]
+		name = ms.Map(name)
+
+		if _, ok := resourceMap[name]; !ok {
+			resourceMap[name] = []terraform_utils.Resource{}
+		}
+		resourceMap[name] = append(resourceMap[name], resource)
+	}
+
+	for name, rs := range resourceMap {
+		fmt.Println(fmt.Sprintf("%s:", name))
+		for _, resource := range rs {
+			name, _ := resource.InstanceState.Attributes["name"]
+			fmt.Println(fmt.Sprintf("\t%s", name))
+		}
+	}
+
+	return nil
 }
