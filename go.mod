12ad19a7ba758f4b665343c532ac67951667a3ff
Kazuhisa Togo <kazuhisa@ktogo.net>
2019-06-14 21:27:13 +0900

    Add error wrapper

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ktogo/terraformer-plan-splitter
 go 1.12
 
 require (
+	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 )
--- plan/io.go
+++ plan/io.go
@@ -4,13 +4,15 @@ import (
 	"encoding/json"
 	"os"
 	"path"
+
+	"github.com/pkg/errors"
 )
 
 // LoadPlanfile parses JSON in a given file path and returns the plan
 func LoadPlanfile(filepath string) (interface{}, error) {
 	f, err := os.Open(filepath)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrapf(err, `plan.LoadPlanfile failed opening '%s'`, filepath)
 	}
 	defer f.Close()
 
@@ -18,7 +20,7 @@ func LoadPlanfile(filepath string) (interface{}, error) {
 	dec := json.NewDecoder(f)
 	dec.DisallowUnknownFields()
 	if err := dec.Decode(plan); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "plan.LoadPlanfile failed decoding JSON")
 	}
 
 	return plan, nil
@@ -27,16 +29,16 @@ func LoadPlanfile(filepath string) (interface{}, error) {
 // ExportPlanfile saves the given plan to the path
 func ExportPlanfile(plan interface{}, filepath string) error {
 	if err := os.MkdirAll(path.Dir(filepath), os.ModePerm); err != nil {
-		return err
+		return errors.Wrapf(err, `plan.ExportPlanfile failed to create a parent directory of '%s'`, filepath)
 	}
 
 	f, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, `plan.ExportPlanfile failed opening '%s'`, filepath)
 	}
 	defer f.Close()
 
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "\t")
-	return enc.Encode(plan)
+	return errors.Wrapf(enc.Encode(plan), "plan.ExportPlanfile failed encoding JSON")
 }
