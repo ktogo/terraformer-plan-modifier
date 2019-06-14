1ea8393a78379459febca05cf1f020cdc89cd577
Kazuhisa Togo <kazuhisa@ktogo.net>
2019-06-14 21:08:41 +0900

    Add argument receiver to split command

--- cmd/split.go
+++ cmd/split.go
@@ -8,9 +8,10 @@ import (
 
 func newCmdSplit() *cobra.Command {
 	return &cobra.Command{
-		Use: "split",
+		Use:  "split",
+		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return fmt.Errorf("Testing")
+			return fmt.Errorf("Testing. First arg is %s", args[0])
 		},
 		SilenceUsage:  true,
 		SilenceErrors: false,
